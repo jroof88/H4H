4916f24a9e04d722064ba217163297afa90e460f
John Roof <jroof@scu.edu>
2017-03-04 14:44:54 -0800

    Seeds template for Aleisha

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+firehouse.create(name: '', age: '', summary: '', photo: '', story: '', goalAmount: '', donationDest: '', locationCounty: '')
+sacred_hearts.create(name: '', age: '', summary: '', photo: '', story: '', goalAmount: '', donationDest: '', locationCounty: '')
+catholic_charities.create(name: '', age: '', summary: '', photo: '', story: '', goalAmount: '', donationDest: '', locationCounty: '')
