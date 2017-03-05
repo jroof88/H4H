bf86b96a902428050eee90e5294ff6d7da49dc9b
John Roof <jroof@scu.edu>
2017-03-04 16:01:20 -0800

    4 o'clock update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-firehouse.create(name: '', age: '', summary: '', photo: '', story: '', goalAmount: '', donationDest: '', locationCounty: '')
-sacred_hearts.create(name: '', age: '', summary: '', photo: '', story: '', goalAmount: '', donationDest: '', locationCounty: '')
-catholic_charities.create(name: '', age: '', summary: '', photo: '', story: '', goalAmount: '', donationDest: '', locationCounty: '')
